1ca73b99dcc350b2af5a13ea408f07654233064f
IanDude <iandudley012@gmail.com>
2024-11-20 09:04:06 +0800

    Added logic for main bottom navigation in MainPage

--- app/src/main/java/com/example/sovereign/MainActivity.java
+++ app/src/main/java/com/example/sovereign/MainActivity.java
@@ -1,25 +1,53 @@
 package com.example.sovereign;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.sovereign.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
+        changeFragment(new HomeFragment());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
+        binding.bottomNavigationMenu.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.Home) {
+                changeFragment(new HomeFragment());
+            } else if (id == R.id.Recognition) {
+                changeFragment(new RecognitionFragment());
+            }
+            return true;
+        });
+
+    }
+
+    protected void changeFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.Mainlayout, fragment);
+        fragmentTransaction.commit();
     }
 
 
