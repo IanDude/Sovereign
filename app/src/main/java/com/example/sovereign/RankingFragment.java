9b6f89e917982e676add1bbbdaba7878a16fa64a
IanDude <iandudley012@gmail.com>
2024-11-26 21:14:19 +0800

    removed unnecessary codes

--- app/src/main/java/com/example/sovereign/RankingFragment.java
+++ app/src/main/java/com/example/sovereign/RankingFragment.java
@@ -1,42 +1,23 @@
 package com.example.sovereign;
 
-import android.content.Intent;
 import android.os.Bundle;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.os.Handler;
-import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.TextView;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Collections;
-   
+
+
 public class RankingFragment extends Fragment {
 
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
-        View view = inflater.inflate(R.layout.fragment_ranking, container, false);
-        ImageButton update_ranking = view.findViewById(R.id.updt_dptm);
-        update_ranking.setOnClickListener(view1 -> new Handler(Looper.getMainLooper()).postDelayed(() -> {
-            Intent intent= new Intent(view1.getContext(), ranking_update.class);
-            startActivity(intent);
-        },1000));
-
-        return view;
-
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ranking, container, false);
     }
-
-
 }
