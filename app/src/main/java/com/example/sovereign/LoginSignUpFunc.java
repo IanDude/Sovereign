946806a1466ed86e47b39c38cd26d402a06bdf62
IanDude <iandudley012@gmail.com>
2024-12-01 09:30:43 +0800

    Fixed logic of methods in Login and Sign Up

--- app/src/main/java/com/example/sovereign/LoginSignUpFunc.java
+++ app/src/main/java/com/example/sovereign/LoginSignUpFunc.java
@@ -3,12 +3,9 @@ package com.example.sovereign;
 import android.content.Context;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicBoolean;
+
 
 public class LoginSignUpFunc {
     protected FirebaseFirestore firebase;
@@ -19,76 +16,77 @@ public class LoginSignUpFunc {
         this.context = context;
     }
 
-    protected void UserSignUp(EditText UserInput, EditText PassInput, Map<String, Object> Collection, HashMap hashMap){
-        AtomicBoolean UserExists = new AtomicBoolean(false);
+    protected void UserSignUp(EditText UserInput, EditText PassInput, String Collection, Map<String, Object> hashMap){
         String Username = UserInput.getText().toString().trim();
         String Password = PassInput.getText().toString().trim();
-        //Check if username already exists
-        firebase.collection(Collection.toString())
+
+        firebase.collection(Collection)
                 .whereEqualTo("Username",Username)
                 .get()
                 .addOnCompleteListener(task -> {
-                    if (task.isSuccessful() && task.getResult() != null && !task.getResult().isEmpty()){ //checks if user is found
-                        UserExists.set(true);
+                    if (task.isSuccessful() && task.getResult() != null && !task.getResult().isEmpty()){
+                        //username already in database
                         Toast.makeText(context.getApplicationContext(),"Invalid Username",Toast.LENGTH_SHORT).show();
                     }else{
-                        UserExists.set(false);
+                        //username not in database, adds the new data
+                        hashMap.put("Username", Username);
+                        hashMap.put("Password", Password);
+                        firebase.collection(Collection)
+                                .add(hashMap)
+                                .addOnSuccessListener(documentReference -> {
+                                    Toast.makeText(context.getApplicationContext(),"Sign Up Successfuil",Toast.LENGTH_SHORT).show();
+                                })
+                                .addOnFailureListener(e -> {
+                                    Toast.makeText(context.getApplicationContext(),"Sign Up Failed" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                                });
                     }
                 }).addOnFailureListener(e -> {
                         Toast.makeText(context.getApplicationContext(),"Error"+ e.getMessage(),Toast.LENGTH_SHORT).show();
                 });
-
-        if (UserExists.equals(false)){
-            hashMap.put("Username", Username);
-            hashMap.put("Password", Password);
-            firebase.collection(Collection.toString())
-                    .add(hashMap)
-                    .addOnSuccessListener(documentReference -> {
-                        Toast.makeText(context.getApplicationContext(),"Sign Up Successfuil",Toast.LENGTH_SHORT).show();
-                    })
-                    .addOnFailureListener(e -> {
-                        Toast.makeText(context.getApplicationContext(),"Sign Up Failed" + e.getMessage(), Toast.LENGTH_SHORT).show();
-                    });
-        }
-
 
     }
     protected boolean EmptyFields(EditText UserInput, EditText PassInput){
         String Username = UserInput.getText().toString().trim();
         String Password = PassInput.getText().toString().trim();
         if (Username.isEmpty() || Password.isEmpty()){
-            Toast.makeText(context.getApplicationContext(),"Please fill all fields.",Toast.LENGTH_SHORT);
             return true;
         }else {
             return  false;
         }
     }
-    protected void UserCheck (EditText UserInput, EditText PassInput, Map<String, Object> Collection){
+    protected void UserLogin (EditText UserInput, EditText PassInput, String Collection, LoginCallback callback){
         String Username = UserInput.getText().toString();
         String Password = PassInput.getText().toString();
-        firebase.collection(Collection.toString())
+        firebase.collection(Collection)
                 .whereEqualTo("Username", Username)
                 .get()
                 .addOnCompleteListener(task -> {
                     if (task.isSuccessful() && task.getResult() != null && !task.getResult().isEmpty() ){
-                        firebase.collection(Collection.toString())
+                        firebase.collection(Collection)
+                                .whereEqualTo("Username",Username)
                                 .whereEqualTo("Password", Password)
                                 .get().addOnCompleteListener(task2 -> {
                                     if (task2.isSuccessful() && task2.getResult() != null && !task2.getResult().isEmpty()){
-
+                                        callback.onLoginSuccess();
                                     }
-                                }).addOnFailureListener(e -> {
-
+                                    else {
+                                        //password doesnt match with username
+                                        callback.onLoginFailure("Incorrect username or password.");
+                                    }
+                                }).addOnFailureListener(e -> { callback.onLoginFailure("Error checking password: " + e.getMessage());
                                 });
-                        Toast.makeText(context.getApplicationContext(),"User already exists.", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(context.getApplicationContext(),"Login Successful.", Toast.LENGTH_SHORT).show();
                     }else{
-                        Toast.makeText(context.getApplicationContext(),"",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(context.getApplicationContext(),"User not found",Toast.LENGTH_SHORT).show();
                     }
                 })
                 .addOnFailureListener(e -> {
-
+                    callback.onLoginFailure("Error checking username: " + e.getMessage());
                 });
     }
-
+    protected interface LoginCallback{
+        void onLoginSuccess();
+        void onLoginFailure(String errorMessage);
+    }
 
 }
